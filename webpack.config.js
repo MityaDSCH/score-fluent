b55430dbab66c890cb3416ff979b6028c8478789
MityaDSCH <mlsgearon@gmail.com>
2016-05-20 12:21:14 -0500

    Added hash to filename and seperated style and vendor bundles; done w/ webpack setup!(?)

--- webpack.config.js
+++ webpack.config.js
@@ -7,31 +7,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const pkg = require('./package.json');
+
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'src/client/app'),
+    style: path.join(__dirname, 'src/client/app/main.scss'),
     build: path.join(__dirname, 'dist/client')
 };
 process.env.BABEL_ENV = TARGET;
 
 const common = {
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
+    style: PATHS.style
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    chunkFilename: '[chunkhash].js'
   },
   module: {
     loaders: [
-      {
-        test: /\.s(c|a)ss$/,
-        loaders: ['style', 'css', 'sass'],
-        include: PATHS.app
-      },
       {
         test: /\.jsx?$/,
         loaders: ['babel?cacheDirectory'],
@@ -52,6 +51,18 @@ const common = {
 // Add dev server config
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
+    output: {
+      filename: '[name].[hash].js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.s(c|a)ss$/,
+          loaders: ['style', 'css', 'sass'],
+          include: PATHS.app
+        }
+      ]
+    },
     devServer: {
       historyApiFallback: true,
       hot: true,
@@ -71,11 +82,33 @@ if (TARGET === 'start' || !TARGET) {
 // Just build it
 if (TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
+    entry: {
+      vendor: Object.keys(pkg.dependencies).filter(function(v) {
+        return v !== 'alt-utils'
+      })
+    },
+    output: {
+      filename: '[name].[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.s(c|a)ss$/,
+          loader: ExtractTextPlugin.extract('style', 'css', 'sass'),
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"'
       }),
       new CleanPlugin([PATHS.build]),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
+      new ExtractTextPlugin('[name].[chunkhash].css'),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
