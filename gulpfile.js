04d98b30e7b096be72bf23e3f555d3ab39cfcd01
MityaDSCH <mlsgearon@gmail.com>
2016-05-22 22:43:28 -0500

    Run dev from gulp becuase fu npm, I hate you so much right now

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require("./webpack.config.js");
 var nodemon = require('gulp-nodemon');
 var del = require('del');
 
@@ -12,7 +16,40 @@ const config = {
   }
 }
 
-gulp.task('clean-dist', function() {
+// ----------------------------------------------------------------------------
+// Webpack
+// ----------------------------------------------------------------------------
+
+var myDevConfig = Object.create(webpackConfig);
+myDevConfig.devtool = "sourcemap";
+myDevConfig.debug = true;
+
+gulp.task("webpack:dev", function(callback) {
+	// modify some webpack config options
+	var myConfig = Object.create(webpackConfig);
+	myConfig.devtool = "eval";
+	myConfig.debug = true;
+
+	// Start a webpack-dev-server
+	new WebpackDevServer(webpack(myConfig), {
+		historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    stats: {colors: true}
+	}).listen(8080, "localhost", function(err) {
+		if(err) throw new gutil.PluginError("webpack-dev-server", err);
+		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+	});
+});
+
+// ----------------------------------------------------------------------------
+// Delete, copy, and watch server
+// ----------------------------------------------------------------------------
+
+gulp.task('del-server', function() {
   return del([
     config.paths.serverDist
   ]);
@@ -35,6 +72,12 @@ gulp.task('nodemon', () => {
   });
 });
 
-gulp.task('build-server', ['clean-dist', 'copy-server']);
+// ----------------------------------------------------------------------------
+// Compose tasks
+// ----------------------------------------------------------------------------
 
-gulp.task('dev-server', ['build-server', 'nodemon']);
+gulp.task('build-server', ['del-server', 'copy-server']);
+
+gulp.task('watch-backend', ['build-server', 'nodemon']);
+
+gulp.task('default', ['webpack:dev', 'watch-backend']);
