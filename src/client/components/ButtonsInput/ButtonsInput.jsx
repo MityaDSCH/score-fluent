d0184b292eb2ef2bff4e6f912c32aa9a8a954bce
MityaDSCH <mlsgearon@gmail.com>
2016-08-22 14:00:35 -0500

    Redo buttons css

--- src/client/components/ButtonsInput/ButtonsInput.jsx
+++ src/client/components/ButtonsInput/ButtonsInput.jsx
@@ -11,23 +11,36 @@ export default class ButtonsInput extends React.Component {
   }
 
   render() {
+
+    const mapButtons = buttons => {
+      return (
+        <div className="row-container">
+          {buttons.map((note, ind) => {
+            // Set button class to var status if it corresponds to (in)correctNote
+            let status = '';
+            if (this.props.correctNote == note) status = ' correct';
+            else if (this.props.incorrectNote == note) status = ' incorrect';
+
+            return (
+              <button
+                key={ind}
+                className={'keyboard-button' + status}
+                onClick={this.props.guessNote.bind(null, note)}>
+                {note}
+              </button>
+            )
+          })}
+        </div>
+      )
+    }
+
+    const row1 = mapButtons(this.props.inputNotes.slice(0, 6));
+    const row2 = mapButtons(this.props.inputNotes.slice(6, 12));
+
     return (
       <div id="keyboard">
-        {this.props.inputNotes.map((note, ind) => {
-          // Set button class to var status if it corresponds to (in)correctNote
-          let status = '';
-          if (this.props.correctNote == note) status = ' correct';
-          else if (this.props.incorrectNote == note) status = ' incorrect';
-
-          return (
-            <button
-              key={ind}
-              className={'keyboard-button' + status}
-              onClick={this.props.guessNote.bind(null, note)}>
-              {note}
-            </button>
-          )
-        })}
+        {row1}
+        {row2}
       </div>
     )
   }
